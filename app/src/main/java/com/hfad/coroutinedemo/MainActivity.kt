18d8a4ecba7f7be46e1df30f94987eba957a6930
Natalia <kovalchuk_natalya2000@mail.ru>
2023-11-18 08:41:22 +0700

    добавление suspend функции

--- app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import com.hfad.coroutinedemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    suspend fun performTask(tasknumber: Int): Deferred<String> =
+        coroutineScope.async(Dispatchers.Main) {
+            delay(5_000)
+            return@async "Finished Coroutine ${tasknumber}"
+        }
+
     private lateinit var binding: ActivityMainBinding
     private var count: Int = 1
 
